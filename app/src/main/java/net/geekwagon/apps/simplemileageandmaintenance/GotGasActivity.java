369f723372c10b91935ac6c86aa4234a2eacc747
James Pryor <james@geekwagon.net>
2015-09-03 11:24:26 -0500

    Initial commit

---
+++ app/src/main/java/net/geekwagon/apps/simplemileageandmaintenance/GotGasActivity.java
@@ -0,0 +1,7 @@
+package net.geekwagon.apps.simplemileageandmaintenance;
+
+/**
+ * Created by james on 2015-09-01.
+ */
+public class GotGasActivity {
+}
---
+++ app/src/main/java/net/geekwagon/apps/simplemileageandmaintenance/MainActivity.java
@@ -0,0 +1,49 @@
+package net.geekwagon.apps.simplemileageandmaintenance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    Button yourButton = (Button) findViewById(R.id.bt_to_screen_gas);
+
+    yourButton.setOnClickListener(new OnClickListener(){
+        public void onClick(View view){
+            startActivity(new Intent(this, GotGasActivity.class));
+        }
+    });
+}
+
